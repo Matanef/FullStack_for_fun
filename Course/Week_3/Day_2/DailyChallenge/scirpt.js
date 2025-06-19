49923f22df816f731df4ad58d63867fb23cf2eca
matanef <efratimatan@gmail.com>
2025-06-19 17:42:09 +0300

    Week_3_Day_2_Daily_challenge

---
+++ Course/Week_3/Day_2/DailyChallenge/scirpt.js
@@ -0,0 +1,23 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+let adjustedarray = fruits.splice(0,1)
+console.log(adjustedarray);
+console.log(fruits);
+
+let sortedArray = fruits.sort()
+console.log(sortedArray);
+
+fruits.push("Kiwi")
+console.log(fruits);
+
+fruits.splice(0,1)
+console.log(fruits);
+
+fruits.reverse()
+console.log(fruits);
+
+
+//excercise 2
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+let oranges = moreFruits[1][1]
+console.log(oranges);
