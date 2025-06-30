af51a0eaf34a37468a5d1e8dbc7c911495ff77f7
matanef <efratimatan@gmail.com>
2025-06-30 07:54:34 +0300

    words in the stars

--- Course/Week_4/Day_2/DailyChallenge/script.js
+++ Course/Week_4/Day_2/DailyChallenge/script.js
@@ -1,37 +1,80 @@
-// function wordsInTheStars() {
-//     let sentence = prompt("Please enter a sentence with words seperated by commas")
+// function wordsInTheStars(words) {
+//     let astlength = words.length;
+//     console.log(astlength);
+    
 //     let stars = "*"
-//     let sentenceArray = sentence.split(",")
+//     let sentenceArray = words.split(",")
 //     console.log(sentenceArray);
-//     for (let i = 0; i < sentenceArray.length; i++) {
-//         const word = sentenceArray[i];
-//         console.log(`*${word}*`);
+//     for (let i = 0; i < astlength.length; i++) {
+//         for (let j = 0; j < sentenceArray.length; j++) {
+//             if (condition) {
+//                 const word = sentenceArray[j];
+//                 return word
+//             }
+//         }
+        
+        
         
 //     }
     
 // }
 
-// wordsInTheStars()
+// let a = wordsInTheStars("Hello,World,in,a,frame")
+// console.log(a);
 
-function wordsInStars(words){
-    let astlength = words.length;
-    let ast = '';
-    let sentenceArray = words
-    const split_string = sentenceArray.split(",");
-    for (let i = 1; i<=astlength; i++){
-        for (let j = 0; j<sentenceArray.split(","); j++){
-            if (","){
-                
-            }
+// function wordsInTheStars(words) {
+//     let sentence = words
+//     console.log(sentence);
+//     let sentenceArray = sentence.split(",")
+//     console.log(sentenceArray);
+//     let longestWordlength = 0
+//     for (let i = 0; i < sentenceArray.length; i++) {
+//         let word = sentenceArray[i]
+        
+//         if (word.length> longestWordlength) {
+//             longestWordlength = word.length
+//             console.log(`the longest word length is: ${longestWordlength}`);
+//         }
+//         let paddedWord = word.padEnd(longestWordlength)
+//         console.log(`* ${paddedWord} *`);
+//         // const element = sentenceArray[i];
+//         // console.log(element);        
+//     }
+//     let border = "*".repeat(longestWordlength + 4)
+//     console.log(border);
+    
+    
+// }
 
+// let a = wordsInTheStars("Hello,World,in,a,frame")
+// console.log(a);
+
+
+function wordsInTheStars(words) {
+    let sentence = words
+    console.log(sentence);
+    let sentenceArray = sentence.split(",")
+    console.log(sentenceArray);
+    let longestWordlength = 0;
+    for (let i = 0; i < sentenceArray.length; i++) {
+        let word = sentenceArray[i];
+        if (word.length > longestWordlength) {
+            longestWordlength = word.length
+            console.log(longestWordlength);  
         }
-        ast +="*";
-        console.log(ast);
-        console.log(astlength);
+        console.log(word);
+    }
+    let border = "*".repeat(longestWordlength + 4)
+    console.log(border);
 
-    return split_string;
-}
+    for (let i = 0; i < sentenceArray.length; i++) {
+        const word = sentenceArray[i];
+        let paddedWord = word.padEnd(longestWordlength)
+        console.log(`* ${paddedWord} *`);
+        
+    }
+    console.log(border);
 }
 
-let a = wordsInStars("Hello, World, in, a, frame")
-console.log(a)
+let a = wordsInTheStars("Hello,World,in,a,frame")
+console.log(a);
