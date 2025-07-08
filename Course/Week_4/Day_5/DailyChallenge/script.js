a685d19858b4ce6374b1891bdaf4cf8c6d6c4167
matanef <efratimatan@gmail.com>
2025-07-08 09:00:44 +0300

    Ternary Operator practice

--- Course/Week_4/Day_5/DailyChallenge/script.js
+++ Course/Week_4/Day_5/DailyChallenge/script.js
@@ -5,6 +5,26 @@ let sentence =  `${remainingBottels} Bottels of beer on the wall`
 
 while (remainingBottels>0) {
     console.log(`${remainingBottels} bottle${remainingBottels === 1 ? '' : 's'} of beer on the wall`);
+    console.log(`${remainingBottels} bottle${remainingBottels === 1 ? '' : 's'} of beer`)
+    
+    
+    let pronoun = subtraction === 1 ? 'it' : 'them';
+    console.log(`Take ${subtraction} down, pass ${pronoun} around`);
+
     remainingBottels -= subtraction
+
+    if (remainingBottels<=0) {
+        console.log('no more bottels on the wall');
+        
+    }else{
+        console.log(`${remainingBottels} bottel${remainingBottels === 1 ? '' : 's'} of beer on the wall`);
+        
+    }
+
+    
+    subtraction++
+
+
+        
     
 }
---
+++ Course/Week_4/Day_5/Practice/script.js
@@ -0,0 +1,17 @@
+let count = 1
+
+console.log(`you have ${count} messege${count ===1 ? "":"s"}.`);
+
+
+count = 5
+console.log(`you have ${count} messege${count ===1 ? "":"s"}.`);
+
+let speed = 130
+console.log(`your speed is ${speed}, ${speed >= 120 ? "Too fast" : "safe speed"}`);
+
+let isLightOn = true
+console.log(`${isLightOn===true?"the light is on" : "The light is off"}`);
+
+
+let score = 77
+console.log(`${score>=90? "A" : score<90 && score>=80? "B": "Needs Improvment"}`);
