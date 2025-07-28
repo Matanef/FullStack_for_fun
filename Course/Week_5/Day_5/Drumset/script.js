c5e06fcffeb712217f186e8bb9f1ff05fc8c840c
matanef <efratimatan@gmail.com>
2025-07-28 21:28:51 +0300

    drumset working - pressing and releasing design

--- Course/Week_5/Day_5/Drumset/script.js
+++ Course/Week_5/Day_5/Drumset/script.js
@@ -43,49 +43,94 @@ keysarea.appendChild(keyL)
 function Boom(){
     let boom = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/boom.wav`);
     boom.play()
+    keyA.style.backgroundColor = "red"
 }
 
-keyA.addEventListener('click', Boom)
+keyA.addEventListener('mousedown', Boom)
+
+keyA.addEventListener("mouseup", function(event) {
+    keyA.style.backgroundColor = "white"
+})
+
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="a") {
-        Boom()        
+        Boom()    
+        keyA.style.backgroundColor = "red"  
+    } 
+      
+})
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="a") {
+        keyA.style.backgroundColor = "white"
     }
+    
 })
 
 function Clap(){
     let clap = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/clap.wav`)
     clap.play()
+    keyS.style.backgroundColor = "red"
 }
 
-keyS.addEventListener("click", Clap)
+keyS.addEventListener("mousedown", Clap)
+keyS.addEventListener("mouseup", function(event){
+    keyS.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="s") {
         Clap()        
+    }
+})
+document.addEventListener("keyup", function(event){
+        if (event.key.toLowerCase() ==="s") {
+        keyS.style.backgroundColor = "white"
     }
 })
 
 function HiHat(){
     let hihat = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/hihat.wav`)
     hihat.play()
+    keyD.style.backgroundColor = "red"
 }
 
-keyD.addEventListener("click", HiHat)
+keyD.addEventListener("mousedown", HiHat)
+
+keyD.addEventListener("mouseup", function(event){
+    keyD.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="d") {
-        HiHat()        
+        HiHat()
+        keyD.style.backgroundColor = "red"        
     }
 })
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="d") {
+        keyD.style.backgroundColor = "white"        
+    }
+})
+
 
 function Kick() {
     let kick = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/kick.wav`)
     kick.play()
+    keyF.style.backgroundColor = "red"
 }
 
-keyF.addEventListener("click", Kick)
+keyF.addEventListener("mousedown", Kick)
+keyF.addEventListener("mouseup", function(event){
+    keyF.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="f") {
         Kick()        
     }
+})
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="f") {
+        keyF.style.backgroundColor = "white"
+    }
+    
 })
 
 function OpenHat() {
@@ -93,57 +138,109 @@ function OpenHat() {
     openhat.play()
 }
 
-keyG.addEventListener("click", OpenHat)
+keyG.addEventListener("mousedown", OpenHat)
+keyG.addEventListener("mouseup", function(){
+    keyG.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="g") {
-        OpenHat()        
+        OpenHat() 
+        keyG.style.backgroundColor = "red"       
     }
+})
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="g") {
+        keyG.style.backgroundColor = "white"
+    }
+    
 })
 
 function Ride() {
     let ride = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/ride.wav`)
     ride.play()
+    keyH.style.backgroundColor = "red" 
 }
 
-keyH.addEventListener("click", Ride)
+keyH.addEventListener("mousedown", Ride)
+keyH.addEventListener("mouseup", function(){
+    keyH.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="h") {
         Ride()        
     }
 })
 
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="h") {
+        keyH.style.backgroundColor = "white"
+    }
+    
+})
+
 function Snare() {
     let snare = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/snare.wav`)
     snare.play()
+    keyJ.style.backgroundColor = "red" 
 }
-keyJ.addEventListener("click", Snare)
+keyJ.addEventListener("mousedown", Snare)
+keyJ.addEventListener("mouseup", function(){
+    keyJ.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="j") {
         Snare()        
     }
+})
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="j") {
+        keyJ.style.backgroundColor = "white"
+    }
+    
 })
 
 
 function Tink() {
     let tink = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/tink.wav`)
     tink.play()
+    keyK.style.backgroundColor = "red" 
 }
 
-keyK.addEventListener("click", Tink)
+keyK.addEventListener("mousedown", Tink)
+keyK.addEventListener("mouseup", function(){
+    keyK.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="k") {
         Tink()        
     }
 })
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="k") {
+        keyK.style.backgroundColor = "white"
+    }
+    
+})
 
 function Tom() {
     let tom = new Audio(`./drumset_setup-master/drumset_setup-master/sounds/tom.wav`)
     tom.play()
+    keyL.style.backgroundColor = "red" 
 }
 
-keyL.addEventListener("click", Tom)
+keyL.addEventListener("mousedown", Tom)
+keyL.addEventListener("mouseup", function(){
+    keyL.style.backgroundColor = "white"
+})
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() ==="l") {
         Tom()        
     }
+})
+
+document.addEventListener("keyup", function(event){
+    if (event.key.toLowerCase() ==="l") {
+        keyL.style.backgroundColor = "white"
+    }
+    
 })
