9237a3b85dac8f026bc776ab32d91c63fb3813c2
matanef <efratimatan@gmail.com>
2025-07-30 19:48:14 +0300

    remodeled the structure of the code, added x icon, checkbox

--- Course/Week_5/Day_5/Todo/script.js
+++ Course/Week_5/Day_5/Todo/script.js
@@ -4,33 +4,96 @@ let button = document.getElementById("submitButton")
 let list = document.getElementsByClassName("list")[0]
 let clearButton = document.getElementById("clear")
 
+
 let todoList = []
+let taskIdCounter = 0
+
+
 
 button.addEventListener("click", addItem)
 
 
 function addItem(event){
     event.preventDefault()
-    let inputValue = input.value
+    let inputValue = input.value.trim()
     let lettersRegex = /^[A-Za-z]+$/
     if (!inputValue || !lettersRegex.test(inputValue)) {
         alert("input invalid")
-    }else if (todoList.includes(inputValue)){
-        alert("this item already exist in the list")
+        return
     }
-    else{
-        todoList.push(inputValue)
-        console.log(todoList);
-        let liList = document.createElement("li");
-        liList.innerHTML = `<input type="checkbox">${todoList.length}. ${inputValue}`;
-        list.appendChild(liList);
+    if (todoList.some(task => task.text.toLowerCase() === inputValue.toLowerCase())){
+        alert("This item already exist in the list")
+        return
     }
+
+
+    let newTask = {
+    task_id: taskIdCounter++,
+    text: inputValue,
+    done: false
+    }
+    todoList.push(newTask)
+    console.log(todoList);
+    
+    let liList = document.createElement("li");
+    liList.setAttribute("data-task-id", newTask.task_id)
+
+    let xIcon = document.createElement("i")
+    xIcon.className = "fa-solid fa-x"
+    xIcon.style.cursor = "pointer"
+    xIcon.style.marginRight = "10px"
+    xIcon.addEventListener("mousedown", function() {
+        deleteTask(newTask.task_id)
+    })
+
+    let checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.addEventListener("change", function(){
+        doneTask(newTask.task_id, checkbox, liList)
+    })
+
+
+let span = document.createElement("span")
+    span.textContent = ` ${newTask.task_id +1}. ${inputValue}`
+
+    liList.appendChild(xIcon)
+    liList.appendChild(checkbox)
+    liList.appendChild(span)
+    list.appendChild(liList)
+
+
+    input.value = ""
 
 }
 
+
 clearButton.addEventListener("click", function() {
-    todoList.length = 0
-    console.log(todoList);
+    todoList = []
     list.innerHTML = ""
+    taskIdCounter = 0
     
 })
+
+
+function deleteTask(taskId){
+    todoList = todoList.filter(task => task.task_id !== taskId)
+    let liToRemove = document.querySelector(`[data-task-id='${taskId}']`)
+    if (liToRemove) liToRemove.remove()
+    console.log(todoList);
+    
+}
+
+function doneTask(taskId, checkbox, liElement) {
+    let task = todoList.find(t => t.task_id === taskId)
+    if (task) {
+        task.done = checkbox.checked
+        if (task.done) {
+            liElement.style.color = "red"
+            liElement.style.textDecoration = "line-through"
+        } else {
+            liElement.style.color = "black"
+            liElement.style.textDecoration = "none"
+        }
+    }
+}
+
